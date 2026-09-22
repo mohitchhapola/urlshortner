481bf7f2d795a33d797953073f17bb20c55f83b9
Mohit Kumar <86540460+mohitchhapola@users.noreply.github.com>
2026-09-22 06:20:44 +0000

    feat: add global exception filter and response interceptor to bootstrap

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule,  } from './app.module.js';
+import { GlobalExceptionFilter } from './common/validation/execption.filter.js';
+import { ResponseInterceptor } from './common/interceptors/response.interceptor.js';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalFilters(new GlobalExceptionFilter());
+  app.useGlobalInterceptors(new ResponseInterceptor());
+
   await app.listen(process.env.PORT ?? 3000);
 }
 await bootstrap();
