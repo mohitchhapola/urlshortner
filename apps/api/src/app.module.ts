741da9825cf6ed89708fe6169ece1b50a1df1d1e
Mohit Kumar <86540460+mohitchhapola@users.noreply.github.com>
2026-09-23 07:21:27 +0000

    feat: add Prisma module and service for database integration

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
+import { PrismaModule } from './common/database/prisma/prisma.module.js';
 
 @Module({
   imports: [
+    PrismaModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- apps/api/src/common/database/prisma/prisma.service.ts
+++ apps/api/src/common/database/prisma/prisma.service.ts
@@ -1,12 +1,25 @@
 import { PrismaClient } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
+import { Injectable, OnModuleDestroy, OnModuleInit } from "@nestjs/common";
 
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit,OnModuleDestroy {
+    constructor() {
 const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL!,
 });
 
-const prisma = new PrismaClient({
-  adapter,
-});
+        super({
+            adapter,
+        });
+    }
 
-export default prisma;
+    async onModuleInit() {
+        await this.$connect();
+    }
+
+    async onModuleDestroy() {
+        await this.$disconnect();
+    }
+}
